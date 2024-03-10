7bf6df5bdae67d874074523c2584d7af3cf53391
nvdhai2003 <nvdhai2003@gmail.com>
2024-03-10 16:47:28 +0700

    routes and controllers

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes")
+
 // HTTP Logger
 app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("combined"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 // Template engine
 app.engine(
@@ -19,18 +27,13 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-// route
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-app.get("/search", (req, res) => {
-  res.render("search");
-});
+// Local host - hosting
+// Action --> Dispatcher --> Function Handler
 
-// 127.0.0.1 -localhost
+// Routes init
+route(app);
+
+// 127.0.0.1 - localhost
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
